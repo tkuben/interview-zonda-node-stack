253def83d8f457049066173f5277cf0615636950
Laura Docherty <ldocherty@zondahome.com>
2023-06-01 11:05:19 +0100

    Updating Web to basic code test
    Remove default angular template code, add Zonda branding and add component for displaying subdivision data

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SubdivisionDataDisplayComponent } from './subdivision-data-display/subdivision-data-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SubdivisionDataDisplayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
